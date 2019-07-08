bafa7053387d57d8ee63eb0c80e057932b717d1a
Kaviraj Gosaye <kamesh_kavi@hotmail.fr>
2019-07-08 16:05:08 +0400

    Interface Added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxx';
+import classes from './Layout.css';
 
 const layout =(props) =>(
 
@@ -8,7 +9,7 @@ const layout =(props) =>(
             Toolbar, SideDrawer,Backdrop
         </div>
 
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
