f3c35fce052735835176251c54740c44baded84e
Kaviraj Gosaye <kamesh_kavi@hotmail.fr>
2019-07-09 10:38:09 +0400

    functional add/remove implemented

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,7 +12,13 @@ const buildControls = (props) =>(
 
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                remove={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+            />
         ))}
     </div>
 );
